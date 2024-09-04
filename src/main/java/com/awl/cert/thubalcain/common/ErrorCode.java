f51da6ed11d5aa790d16f78f73924a72a9da5765
constant94-dev <constant94@naver.com>
2024-09-04 22:48:06 +0900

    [DEV/240904] 불필요 파일 삭제 및 ErrorCode 추가

--- src/main/java/com/awl/cert/thubalcain/common/ErrorCode.java
+++ src/main/java/com/awl/cert/thubalcain/common/ErrorCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     NO_SUCH_ALGORITHM(500, "Hashing 알고리듬을 찾을 수 없습니다."),
-    INVALID_KEY_SPEC(500, "유효하지 않은 KeySpec 입니다.");
+    INVALID_KEY_SPEC(500, "유효하지 않은 KeySpec 입니다."),
+    FAILED_CREATE_USER_DB(500, "DB에 사용자가 저장되지 않았습니다.");
 
     private final int code;
     private final String reason;
